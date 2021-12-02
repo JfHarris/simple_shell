d8e49b2b42dd001c0ed34c8efedd291d0b009e26
JfHarris <3374@holbertonschool.com>
2021-12-02 09:31:05 -0800

    Edit main.h

--- main.h
+++ main.h
@@ -8,15 +8,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#define TK_DELM " \t\r\n\a"
 
 void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
+void new_printenv(char **environ);
+void new_input(void);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 void _puts(char *str);
 char *_strdup(char *str);
-char *_itoa(int value, char *newString);
-
+char *_itoa(long int num, int base);
+char **check_path(char **environ);
+void emer_stop(int signal);
+int exec_comm(char **args, char **av, char **env, int status, int total);
+char **find_path(char *ourpath);
+char **parse_inp(char *line, char **env);
+char *read_comm(void);
+int *_error(char *argv, int total, char *args);
+int new_status(char **comm, char **site);
 
 #endif
